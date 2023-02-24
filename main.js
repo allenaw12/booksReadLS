8d235be6576a4f0b67a0b9333197320df1b49487
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-02-23 22:53:11 -0600

    basic functionality of fetching books with search by author or title, adding elements to dom and functioning next and prev buttons

--- main.js
+++ main.js
@@ -1,11 +1,53 @@
 
-fetch('https://www.googleapis.com/books/v1/volumes?q=intitle:jurassic%20park%20survivor&startIndex=0&maxResults=20')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        console.log(data.items)
-        data.items.forEach(obj => {
-            let li = document.createElement('li')
-            li.innerText = obj.volumeInfo.title
-            document.querySelector('ul').appendChild(li)})
-    })
+
+
+document.querySelector('form').addEventListener('submit', getBooks)
+    
+
+function getBooks(e, start = 0){
+    e ? e.preventDefault() : null
+    let input = document.querySelector('#search').value
+    let filter = document.querySelector('#choice').value 
+    console.log(input, ',', filter, ',', start)
+    document.querySelectorAll('li').forEach(el => el.remove())
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=in${filter}:${input}&startIndex=${start}&maxResults=10&key=AIzaSyB8KFJLCXfinVWzxfnxcmyiT7f-XsmXd2Q`)
+        .then(res => res.json())
+        .then(data => {
+            let total = data.totalItems
+            console.log(data, total)
+            data.items.forEach((obj,i)=> {
+                let img = document.createElement('img')
+                let li = document.createElement('li')
+                if(i==0){
+                    li.classList.add('counter')
+                    li.value = start+1
+                }
+                img.classList.add('thumbnail')
+                img.src= obj.volumeInfo.imageLinks?.smallThumbnail || obj.volumeInfo.imageLinks?.thumbnail || null
+                li.innerText = obj.volumeInfo.title
+                li.appendChild(img)
+                document.querySelector('ol').appendChild(li)})
+            for(i=0;i<total;i+=10){
+                let li = document.createElement('li')
+                let a = document.createElement('a')
+                a.href = `javascript:getBooks(${null}, ${i})`
+                a.innerText = `${i}`
+                li.appendChild(a)
+                document.querySelector('#pages').appendChild(a)
+            }
+        })
+        .catch(err => console.log(`Error: ${err}`))
+}
+
+document.querySelector('#next').addEventListener('click',next)
+function next(e){
+    e ? e.preventDefault() : null
+    let start = +document.querySelector('.counter').value
+    return getBooks(null, start+9)
+}
+document.querySelector('#prev').addEventListener('click',previous)
+function previous(e){
+    e ? e.preventDefault() : null
+    let start = +document.querySelector('.counter').value
+    return getBooks(null, start-11)
+}

34e2736594a62b6f75b8df9bffd2f1f66e8af0e8
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-02-25 19:32:41 -0600

    functionality for author and title search, improved precise search parameters

--- main.js
+++ main.js
@@ -5,43 +5,57 @@ document.querySelector('form').addEventListener('submit', getBooks)
     
 
 function getBooks(e, start = 0, max = 10){
-    e ? e.preventDefault() : null
+    e ? e.preventDefault() : ''
     let input = document.querySelector('#search').value
-    let filter = document.querySelector('#choice').value 
+    let filter = document.querySelector('#choice').value
+    if(filter.includes('-')){
+        let both = input.split(', ')
+        filter = 'author'
+        input = both[0] + '"+intitle:"' + both[1]
+    }
     //console.log(input, ',', filter, ',', start)
     document.querySelectorAll('li').forEach(el => el.remove())
     document.querySelectorAll('a').forEach(el => el.remove())
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=in${filter}:${input}&startIndex=${start}&maxResults=${max}&key=AIzaSyB8KFJLCXfinVWzxfnxcmyiT7f-XsmXd2Q`)
+    // document.querySelector('#pages').innerText = ''
+    let fetching = `https://www.googleapis.com/books/v1/volumes?q=in${filter}:"${input.includes(' ') ? input.split(' ').join('%20'):input}"&startIndex=${start}&maxResults=${max}&key=AIzaSyB8KFJLCXfinVWzxfnxcmyiT7f-XsmXd2Q`
+    console.log(fetching)
+    fetch(fetching)
         .then(res => res.json())
         .then(data => {
             let total = data.totalItems
             console.log(data)
             data.items.forEach((obj,i)=> {
-                console.log('title: ', obj.volumeInfo.title, ',\n',
-                            'subtitle: ', obj.volumeInfo?.subtitle, ',\n',
-                            'authors: ',obj.volumeInfo.authors?.join(', '), ',\n',
-                             'categories: ',obj.volumeInfo.categories?.join(', '), ',\n',
-                             'description: ', obj.volumeInfo.description || obj.searchInfo?.textSnippet || null, ',\n',
-                             'google book listing: ',`https://books.google.com/books?id=${obj.id}`, ',\n',
-                             'pages: ', obj.volumeInfo?.pageCount, ',\n',
-                             obj.volumeInfo)
+                // console.log('title: ', obj.volumeInfo.title, ',\n',//
+                //             'subtitle: ', obj.volumeInfo?.subtitle, ',\n',//
+                //             'authors: ',obj.volumeInfo.authors?.join(', '), ',\n',
+                //             'categories: ',obj.volumeInfo.categories?.join(', '), ',\n',
+                //             'description: ', obj.volumeInfo.description || obj.searchInfo?.textSnippet || 'No description available', ',\n',
+                //             'google book listing: ',`https://books.google.com/books?id=${obj.id}`, ',\n',
+                //             'pages: ', obj.volumeInfo?.pageCount, ',\n',
+                //              obj.volumeInfo)
                 let img = document.createElement('img')
                 let li = document.createElement('li')
+                let title = document.createElement('span')
                 if(i==0){
                     li.classList.add('counter')
                     li.value = start+1
                 }
                 img.classList.add('thumbnail')
                 img.src= obj.volumeInfo.imageLinks?.smallThumbnail || obj.volumeInfo.imageLinks?.thumbnail || null
-                li.innerText = obj.volumeInfo.title
                 li.appendChild(img)
+                title.innerText = 'Title: ' + obj.volumeInfo.title + (obj.volumeInfo.subtitle ? ': ' + obj.volumeInfo.subtitle : '')
+                li.appendChild(title)
                 document.querySelector('ol').appendChild(li)})
+            let pageLinks = 1
             for(i=0;i<total;i+=10){
-                let li = document.createElement('li')
+                // if(i===0 && pageLinks>1){
+                //     console.log('pages greater than 1', pageLinks)
+                //     pageLinks = 1}
                 let a = document.createElement('a')
                 a.href = `javascript:getBooks(${null}, ${i})`
-                a.innerText = `${i}`
-                li.appendChild(a)
+                a.innerText = `${pageLinks}`
+                pageLinks++
+                console.log(pageLinks)
                 document.querySelector('#pages').appendChild(a)
             }
         })
@@ -50,13 +64,13 @@ function getBooks(e, start = 0, max = 10){
 
 document.querySelector('#next').addEventListener('click',next)
 function next(e){
-    e ? e.preventDefault : null
+    e ? e.preventDefault : ''
     let start = +document.querySelector('.counter').value
     return getBooks(null, start+9)
 }
 document.querySelector('#prev').addEventListener('click',previous)
 function previous(e){
-    e ? e.preventDefault : null
+    e ? e.preventDefault : ''
     let start = +document.querySelector('.counter').value
     return getBooks(null, start-11)
 }

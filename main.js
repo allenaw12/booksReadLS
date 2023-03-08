09a891d491a2d48d14eb956fb6dc867eb940310e
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-03-07 19:05:00 -0600

    commenting lines to remember what they do, deleted some console.logs

--- main.js
+++ main.js
@@ -1,32 +1,46 @@
-//suggestion to change string from outerhtml into local storage into a js object and then parse back out...so like take what's in the innertext and put in local and then use to display when lists are requested...
-//could use fetch function to build, just check if need a fetch or not...need to refactor whole function for it, but it can be done!
-
+//fetch results from API with search button or enter key press
 document.querySelector('form').addEventListener('submit', getBooks)
+//get books and create display cards
 function getBooks(e, start = 0, max = 10){
     e ? e.preventDefault() : ''
+    //search term
     let input = document.querySelector('#search').value
+    //if nothing, don't ping the API!
     if(input === ''){return document.querySelector('#error').innerText = 'Please type a query into search field.'}
+    //what want to search within, author, title etc
     let filter = document.querySelector('#choice').value
+    //isbn search slightly different than precise othe searches
     filter === 'isbn' ? input : input = `"${input}"`
+    //concatenate fetch for a search of both author and title
     if(filter.includes('-')){
         let both = input.split(',')
         filter = 'author'
         input = `"${both[1].trim()}intitle:${both[0].trim()}"`
     }
+    //resetting values before delivering search results
     document.querySelector('#my-lists').value = 'my-lists'
     document.querySelectorAll('li').forEach(el => el.remove())
     document.querySelectorAll('a').forEach(el => el.remove())
     document.querySelector('#error').innerText = ''
+    //putting string of fetch site together
     let fetching = `https://www.googleapis.com/books/v1/volumes?q=${filter}:${input.includes(' ') ? input.split(' ').join('%20'):input}&startIndex=${start}&maxResults=${max}&key=AIzaSyB8KFJLCXfinVWzxfnxcmyiT7f-XsmXd2Q`
     console.log(fetching)
+    //fetch from API
     fetch(fetching)
+        //get response into JSON data
         .then(res => res.json())
+        //use that data!
         .then(data => {
+            //total items found matching search
             let total = data.totalItems
             console.log(data)
+            //displaying total in DOM
             document.querySelector('.total').innerText = total
+            //error display for no results found
             if(total === 0 || total === undefined) return document.querySelector('#error').innerText = data.error ? `Error code: ${data.error.code} Message: ${data.error.message}`:'No Results Found'
+            //create 'cards' to display books matching search
             data.items.forEach((obj,i)=> {
+                //declare/create elements to make card
                 let div = document.createElement('div')
                 let local = document.createElement('span')
                 let read = document.createElement('span')
@@ -41,43 +55,53 @@ function getBooks(e, start = 0, max = 10){
                 let description = document.createElement('p')
                 let pages = document.createElement('span')
                 let isbn = document.createElement('span')
+                //setting counter so results list can be numerated
                 if(i==0){
                     li.classList.add('counter')
                     li.value = start+1
                 }
+                //saving volume specific identifiers by iterating thru object
                 let texts = obj.volumeInfo.industryIdentifiers.map(el => `${el.type}: ${el.identifier}`)
                 //link to JSON data for book = obj.selfLink
                 //link to buy ebook or google books information = obj.volumeInfo.canonicalVolumeLink, obj.volumeInfo.infoLink
                 //link to preview or google books info = obj.volumeInfo.previewLink
                 console.log(obj.selfLink)
+                //concatenating identifiers into single line to save/display
                 isbn.innerText = texts.join(', ')
+                //declareing and adding classes to heart icon for read list and bookmark icon for tbr list
                 let readClasses = `id${obj.id}` + ' read fa-regular fa-heart'
                 let tbrClasses = `id${obj.id}` + ' tbr fa-regular fa-bookmark'
                 readClasses.split(' ').forEach(el => read.classList.add(el))
                 tbrClasses.split(' ').forEach(el => tbr.classList.add(el))
+                //adding a class to the outer div element
                 div.classList.add('container')
-                // li.classList.add(`id${obj.id}`)
+                //adding id to list element to differeniate from other li elements
                 li.setAttribute('id',`id${obj.id}`)
-                // title.classList.add(obj.id)
+                //appending icons to holding element
                 local.appendChild(read)
                 local.appendChild(tbr)
-                // local.appendChild(trash)
+                //appending icons holder to bigger book container
                 div.appendChild(local)
-                // li.appendChild(local)
+                //creating link to book listing in API
                 link.href = `https://books.google.com/books?id=${obj.id}`
+                //open link in separate window
                 link.target = '_blank'
+                //adding classes
                 art.classList.add('information')
                 img.classList.add('thumbnail')
                 description.classList.add('desc-short')
+                //adding src to thumbnail image
                 img.src = obj.volumeInfo.imageLinks?.thumbnail ||  obj.volumeInfo.imageLinks?.smallThumbnail ||'NoBookCover.png'
+                //appending image and link elements to bigger book container
                 link.appendChild(img)
                 div.appendChild(link)
-                // li.appendChild(link)
+                //setting title/author/genre/discription/pages display with what's provide, or none available
                 title.innerText = `Title: ${obj.volumeInfo.title || 'None provided'}${obj.volumeInfo.subtitle ? `: ${obj.volumeInfo.subtitle}` : ''}`
                 author.innerText = `Author(s): ${obj.volumeInfo.authors?.join(', ') || 'None provided'}`
                 genre.innerText = `Genre(s): ${obj.volumeInfo.categories?.join(', ') || 'None provided'}`
                 description.innerText = `Description: ${obj.volumeInfo.description || obj.searchInfo?.textSnippet || 'None available'}`
                 pages.innerText = `Pagecount: ${obj.volumeInfo?.pageCount || 'Not available'}`
+                //appending all elements in order desired to text container
                 art.appendChild(title)
                 art.appendChild(author)
                 art.appendChild(genre)
@@ -99,28 +123,37 @@ function getBooks(e, start = 0, max = 10){
                 for(let child of art.children){
                     child.classList.add('descriptor')
                 }
+                //append text holding container to bigger book container and then that to the li and then to the ol!
                 div.appendChild(art)
                 li.appendChild(div)
-                // li.appendChild(art)
                 document.querySelector('ol').appendChild(li)
             })
+            //adding event listener to read icons for click event
             document.querySelectorAll('.read').forEach(li => li.addEventListener('click',() => {
+                //get classes of list element holding icon
                 let first  = li.attributes[0].value.split(' ')
+                //getting entire li element and all it's HTML
                 let string = document.querySelector(`li#${first[0]}`).outerHTML
+                //editing html if class counter is detected, and removing that class
                 if(string.indexOf('class="counter"') > -1){
                     console.log('counter in element')
                     let end = string.indexOf('id')
                     string = string.slice(0,4) + string.slice(end)
                 }
+                //making array of indices of spans in html string
                 let spans = [...string.matchAll('span')]
+                //creating a class variable to add to delete icon being added to read and tbr elements
                 let trashClasses = `${first[0]}` + ' delete fa-regular fa-trash-can'
-                // console.log(string.slice(0, spans[1].index-1)+`<span class="${trashClasses}"></span>`+string.slice(spans[4].index+5))
+                //editing html string to have delete icon
                 string = string.slice(0, spans[1].index-1)+`<span class="${trashClasses}"></span>`+string.slice(spans[4].index+5)
-                //console.log(string)
+                //getting or setting local storage to save book in read list
                 let storage = localStorage.getItem('read') ? JSON.parse(localStorage.getItem('read')) : []
+                //adding book to string gotten from local storage
                 storage.push(string)
+                //setting localstorage with new book added to read list
                 localStorage.setItem('read', JSON.stringify(storage))
             }))
+            //add event listener to bookmark icon to add tbr list on click
             document.querySelectorAll('.tbr').forEach(li => li.addEventListener('click',() => {
                 let first  = li.attributes[0].value.split(' ')
                 let string = document.querySelector(`li#${first[0]}`).outerHTML
